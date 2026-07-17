c9cf96190799097291b7fb2d1f5f6adc9a4c666a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    small refactor - ModalJsonExtensions

--- Components/Modal/ModalJsonExtensions.cs
+++ Components/Modal/ModalJsonExtensions.cs
@@ -9,70 +9,66 @@ using System.Text.Json.Serialization.Metadata;
 /// </summary>
 public static class ModalJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+	};
 
-    /// <summary>
-    /// Serializes the modal to a JSON string.
-    /// </summary>
-    /// <param name="value">The modal instance to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the modal.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
-    public static string ToJson(this Modal value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the modal to a JSON string.
+	/// </summary>
+	/// <param name="value">The modal instance to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the modal.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
+	public static string ToJson(this Modal value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string into a new <see cref="Modal"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A new modal instance populated from the JSON, or null if deserialization fails.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
-    public static Modal? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+	/// <summary>
+	/// Deserializes a JSON string into a new <see cref="Modal"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>A new modal instance populated from the JSON, or null if deserialization fails.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static Modal? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        try
-        {
-            return JsonSerializer.Deserialize<Modal>(json, _jsonOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+		return JsonSerializer.Deserialize<Modal>(json, _jsonOptions);
+	}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string into a <see cref="Modal"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized modal instance if successful; otherwise, null.</param>
-    /// <returns>True if deserialization succeeded; false if the JSON is invalid or deserialization failed.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
-    public static bool TryFromJson(string json, out Modal? value)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+	/// <summary>
+	/// Attempts to deserialize a JSON string into a <see cref="Modal"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized modal instance if successful; otherwise, null.</param>
+	/// <returns>True if deserialization succeeded; false if the JSON is invalid or deserialization failed.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+	/// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
+	public static bool TryFromJson(string json, out Modal? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        try
-        {
-            value = FromJson(json);
-            return value is not null;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+		try
+		{
+			value = FromJson(json);
+			return value is not null;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
+
 }
