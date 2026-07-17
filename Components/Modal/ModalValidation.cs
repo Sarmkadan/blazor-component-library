cf4691152f15b6bfcc043b7f7ffdc61e664406e1
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Tighten guard clauses in ModalValidation.

--- Components/Modal/ModalValidation.cs
+++ Components/Modal/ModalValidation.cs
@@ -2,61 +2,58 @@ namespace BlazorComponentLibrary.Components.Modal;
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 public static class ModalValidation
 {
     /// <summary>
-    /// Validates the given <paramref name="value"/> and returns a list of human-readable problems.
+    /// Validates the given <paramref name="modal"/> and returns a list of human-readable problems.
     /// </summary>
-    /// <param name="value">The <see cref="Modal"/> to validate.</param>
+    /// <param name="modal">The <see cref="Modal"/> to validate.</param>
     /// <returns>A list of human-readable problems.</returns>
-    public static IReadOnlyList<string> Validate(this Modal value)
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="modal"/> is <see langword="null"/>.</exception>
+    public static IReadOnlyList<string> Validate(this Modal modal)
     {
-        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(modal);
 
         var problems = new List<string>();
 
-        if (string.IsNullOrEmpty(value.Title))
+        if (string.IsNullOrWhiteSpace(modal.Title))
         {
             problems.Add("Title is required.");
         }
 
-        if (value.CloseOnOverlayClick && value.OnClose.HasDelegate)
+        if (modal.CloseOnOverlayClick && modal.OnClose.HasDelegate)
         {
             problems.Add("Cannot set CloseOnOverlayClick to true when OnClose is set.");
         }
 
-        return problems;
+        return problems.AsReadOnly();
     }
 
     /// <summary>
-    /// Checks if the given <paramref name="value"/> is valid.
+    /// Checks if the given <paramref name="modal"/> is valid.
     /// </summary>
-    /// <param name="value">The <see cref="Modal"/> to check.</param>
-    /// <returns>True if the <paramref name="value"/> is valid, false otherwise.</returns>
-    public static bool IsValid(this Modal value)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        return !Validate(value).Any();
-    }
+    /// <param name="modal">The <see cref="Modal"/> to check.</param>
+    /// <returns>True if the <paramref name="modal"/> is valid, false otherwise.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="modal"/> is <see langword="null"/>.</exception>
+    public static bool IsValid(this Modal modal) => modal is not null && !Validate(modal).Any();
 
     /// <summary>
-    /// Ensures that the given <paramref name="value"/> is valid.
+    /// Ensures that the given <paramref name="modal"/> is valid.
     /// </summary>
-    /// <param name="value">The <see cref="Modal"/> to ensure is valid.</param>
-    /// <exception cref="ArgumentException">Thrown if the <paramref name="value"/> is invalid.</exception>
-    public static void EnsureValid(this Modal value)
+    /// <param name="modal">The <see cref="Modal"/> to ensure is valid.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="modal"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">Thrown if the <paramref name="modal"/> is invalid.</exception>
+    public static void EnsureValid(this Modal modal)
     {
-        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(modal);
 
-        var problems = Validate(value);
+        var problems = Validate(modal);
 
-        if (problems.Any())
+        if (problems.Count > 0)
         {
-            throw new ArgumentException($"The following problems were found: {string.Join(", ", problems)}", nameof(value));
+            throw new ArgumentException($"The following problems were found: {string.Join(", ", problems)}", nameof(modal));
         }
     }
 }
