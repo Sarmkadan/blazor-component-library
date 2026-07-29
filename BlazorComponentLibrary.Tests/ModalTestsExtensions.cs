e9b8cc236bf39879e60ae75b82abbf533f046b7c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 10:50:58 +0300

    wip

---
+++ BlazorComponentLibrary.Tests/ModalTestsExtensions.cs
@@ -0,0 +1,84 @@
+public static class ModalTestsExtensions
+{
+    // Implement your extension methods here
+    public void DefaultRender_HasDefaultValues
+    {
+        // implement me
+    }
+    public void Title_Set_RendersCorrectTitle
+    {
+        // implement me
+    }
+    public void Title_NullOrEmpty_DoesNotThrow
+    {
+        // implement me
+    }
+    public void ChildContent_Set_RendersContent
+    {
+        // implement me
+    }
+    public void FooterContent_Set_RendersFooter
+    {
+        // implement me
+    }
+    public void FooterContent_Null_DoesNotRenderFooter
+    {
+        // implement me
+    }
+    public void CloseOnOverlayClick_DefaultsToTrue
+    {
+        // implement me
+    }
+    public void CloseOnOverlayClick_False_PreventsOverlayClose
+    {
+        // implement me
+    }
+    public void Size_DefaultsToMedium
+    {
+        // implement me
+    }
+    public void Size_Small_RendersSmallClass
+    {
+        // implement me
+    }
+    public void Size_Large_RendersLargeClass
+    {
+        // implement me
+    }
+    public void Size_FullScreen_RendersFullScreenClass
+    {
+        // implement me
+    }
+    public void IsVisible_DefaultsToFalse
+    {
+        // implement me
+    }
+    public async Task Show_SetsIsVisibleToTrue
+    {
+        // implement me
+    }
+    public async Task Hide_SetsIsVisibleToFalse
+    {
+        // implement me
+    }
+    public async Task Show_MultipleTimes_IsVisibleRemainsTrue
+    {
+        // implement me
+    }
+    public async Task Hide_MultipleTimes_IsVisibleRemainsFalse
+    {
+        // implement me
+    }
+    public async Task ShowAndHide_Sequence_WorksCorrectly
+    {
+        // implement me
+    }
+    public void Parameters_ChangedAfterInitialization_ReflectsChanges
+    {
+        // implement me
+    }
+    public void SizeClass_ReturnsCorrectClass
+    {
+        // implement me
+    }
+}
